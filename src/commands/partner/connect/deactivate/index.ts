7cab76070e65b89a57abb344b55882aa11cea9a5
alouie-sfdc <arthur.louie@salesforce.com>
2026-03-13 12:11:13 -0700

    Remove unused Flags import

--- src/commands/partner/connect/deactivate/index.ts
+++ src/commands/partner/connect/deactivate/index.ts
@@ -1,4 +1,4 @@
-import {Args, Flags} from '@oclif/core'
+import {Args} from '@oclif/core'
 
 import BaseCommand from '../../../../lib/base'
 
@@ -15,7 +15,7 @@ It also destroys any existing Heroku Connect add-ons that are associated with th
   ]
 
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(Deactivate)
+    const {args} = await this.parse(Deactivate)
     const {id_or_slug: idOrSlug} = args
 
     // TODO: Implement deactivation logic
